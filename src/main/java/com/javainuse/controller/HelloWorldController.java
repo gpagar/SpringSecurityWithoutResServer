a07f30e721948fbaae68bf8857f096d6de8d625e
Ganesh <Ganesh@SR-10C-Ganesh>
2019-11-16 11:48:30 +0530

    //Ganesh

---
+++ src/main/java/com/javainuse/controller/HelloWorldController.java
@@ -0,0 +1,23 @@
+package com.javainuse.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+	@RequestMapping({ "/hello" })
+	public String firstPage() {
+		return "Hello World";
+	}
+
+	
+	
+	@RequestMapping({ "/getData" })
+	public String getData() {
+		return "Hello World";
+	}
+
+	
+	
+}
